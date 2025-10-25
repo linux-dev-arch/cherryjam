ef16ebd18835c514513dcd6769b5da9a04958c45
linux-dev <160027919+linux-dev-arch@users.noreply.github.com>
2025-10-25 15:19:00 +0530

    Add files via upload

---
+++ cherryjam.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import font as tkFont
+import os,platform,ctypes
+if platform.system() =="Windows":
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+file_path=""
+# Functions
+def open_file():
+    global file_path
+    file_path = filedialog.askopenfilename(filetypes=[("CherryScript Files", "*.ci")])
+    if file_path:
+        with open(file_path, "r") as File:
+            text_widget.delete("1.0", tk.END)
+            text_widget.insert(tk.END, File.read())
+
+def save_file():
+    global file_path
+    if file_path !="":
+        with open(file_path, "w") as File:
+            File.write(text_widget.get("1.0", tk.END))
+    else:
+        save_file_as()
+def save_file_as():
+    global file_path
+    file_path = filedialog.asksaveasfilename(defaultextension=".ci", filetypes=[("CherryScript Files", "*.ci")])
+    with open(file_path, "w") as File:
+        File.write(text_widget.get("1.0", tk.END))
+def run_file():
+    if platform.system()=="Linux":
+        os.system("konsole --hold -e python3 cherryscript_rolling.py "+file_path)
+    if platform.system()=="Windows":
+        os.system('start cmd /k python.exe cherryscript_rolling.py '+file_path)
+# Root window
+root = tk.Tk()
+root.title("CherryJam")
+root.geometry("1280x720")
+root.configure(bg="#282a36")  # Dracula background
+custom_font = tkFont.Font(family="Arial", size=14)
+# Button frame
+button_frame = tk.Frame(root, bg="#282a36")
+button_frame.pack(side=tk.TOP, anchor="nw", padx=5, pady=5)
+
+button_style = {"bg": "#44475a", "fg": "#f8f8f2", "activebackground": "#6272a4", "activeforeground": "#f8f8f2", "bd":0}
+
+open_button = tk.Button(button_frame, text="Open File", command=open_file, **button_style)
+open_button.pack(side=tk.LEFT, padx=5)
+save_button = tk.Button(button_frame, text="Save File", command=save_file, **button_style)
+save_button.pack(side=tk.LEFT, padx=5)
+save_button = tk.Button(button_frame, text="Save File As", command=save_file_as, **button_style)
+save_button.pack(side=tk.LEFT, padx=5)
+run_button = tk.Button(button_frame, text="Run File", command=run_file, **button_style)
+run_button.pack(side=tk.LEFT, padx=5)
+# Text widget with scrollbar
+text_frame = tk.Frame(root, bg="#282a36")
+text_frame.pack(fill=tk.BOTH, expand=True)
+
+scrollbar = tk.Scrollbar(text_frame, bg="#44475a", troughcolor="#282a36")
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+text_widget = tk.Text(text_frame, wrap=tk.WORD, yscrollcommand=scrollbar.set,
+                      bg="#282a36", fg="#f8f8f2", insertbackground="#f8f8f2",
+                      selectbackground="#44475a", selectforeground="#f8f8f2")
+text_widget.config(font=custom_font,fg="#ff5e5e")
+text_widget.pack(fill=tk.BOTH, expand=True)
+
+scrollbar.config(command=text_widget.yview)
+
+root.mainloop()
